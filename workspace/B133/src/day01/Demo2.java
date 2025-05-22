38d995a2245be0970e65318b227681c1fac59186
AksharaBhanu <javagalbhanu@gmail.com>
2025-05-22 08:58:19 +0530

    Day01

---
+++ workspace/B133/src/day01/Demo2.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Demo2 {
+
+	public static void m1(int i)
+	{
+		System.out.println(i);
+	}
+	
+	public static void main(String[] args) {
+	 int i;
+	 i=10;
+	 System.out.println(i);
+	 
+	 Demo2.m1(10);
+	}
+
+}
